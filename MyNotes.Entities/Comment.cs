b8c4c3b123521429a0a7e21fe61fcbf9e80b09b3
Mehmet OZÇELIK <mehmetozcelik841@gmail.com>
2019-07-24 21:51:59 +0300

    Entities + Settings

---
+++ MyNotes.Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.Entities
+{
+    public class Comment : MyEntityBase
+    {
+        public string Text { get; set; }
+
+
+        public virtual Note Note { get; set; }
+        public virtual MyNotesUser Owner { get; set; }
+
+
+    }
+}
---
+++ MyNotes.Entities/Liked.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.Entities
+{
+    public class Liked
+    {
+        public int Id { get; set; }
+
+
+        public virtual Note Note { get; set; }
+        public virtual MyNotesUser LikedUser { get; set; }
+
+    }
+}
