e21ba0bad44307c64a71ff9b70690e118940e65f
Mehmet OZÇELIK <mehmetozcelik841@gmail.com>
2019-07-30 22:20:26 +0300

    Üyelik İşlemleri

--- MyNotes.WebApp/Controllers/HomeController.cs
+++ MyNotes.WebApp/Controllers/HomeController.cs
@@ -5,48 +5,100 @@ using System.Web;
 using System.Web.Mvc;
 using MyNotes.Entities;
 using MyNotes.BusinessLayer;
+using MyNotes.WebApp.ViewModels;
 
 namespace MyNotes.WebApp.Controllers
 {
     public class HomeController : Controller
-    {
+    {        
         CategoryManager cm = new CategoryManager();
         NoteManager nm = new NoteManager();
 
         /// //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
+
 
         public ActionResult Index()
-        {             
+        {
             return View();
         }
 
         public PartialViewResult PartialNotes()
         {
-            List<Note> list = nm.GetNotes();
-            
+            List<Note> list = nm.GetNotes().OrderByDescending(x => x.ModifiedOn).ToList();
+
             return PartialView("_PartialNotes", list);
         }
 
 
         public PartialViewResult CategorySelect(int? id)
         {
-            List<Note> list=null;
+            List<Note> list = null;
 
             if (id != null)
             {
-                list = nm.GetNotesParam(id);               
+                list = nm.GetNotesParam(id).OrderByDescending(x => x.ModifiedOn).ToList();
             }
             return PartialView("_PartialNotes", list);
         }
 
-        public ActionResult CategoryName(int id)
+        public PartialViewResult CategoryName(int id)
         {
             Category category = cm.FindCategory(id);
+            
+            return PartialView("_PartialHeaderCategory", category);
+        }
 
-            return View("Index",category);
+        public PartialViewResult MostLike()
+        {
+            List<Note> list = nm.GetNotes().OrderByDescending(x => x.LikeCount).ToList();
+            return PartialView("_PartialNotes", list);
+        }
+        
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            //Giriş kontrolü ve yönlendirme.
+            //Session'a kullanıcı bilgileri saklanması.
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            //Kullanıcı e-mail ve username kotrolü.
+            //Kayıt işlem.
+            //Activasyon e-postası gönderimi.
+            return View();
+        }
+
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+
+        public ActionResult UserActivate(Guid guid)
+        {
+            //Kullanıcı aktivasyonu  sağlanacak.
+            return View();
         }
 
     }
-    
 }
---
+++ MyNotes.WebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyNotes.WebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [DisplayName("User name"), Required(ErrorMessage ="{0} alanı boş geçilemez."), 
+            StringLength(25,ErrorMessage ="{0} max. {1} karakter olmalı.")]
+        public string Username { get; set; }
+
+        [DisplayName("Password"), Required(ErrorMessage = "{0} alanı boş geçilemez."), DataType(DataType.Password),
+            StringLength(25, ErrorMessage = "{0} max. {1} karakter olmalı.")]
+        public string Password { get; set; }
+    }
+}
